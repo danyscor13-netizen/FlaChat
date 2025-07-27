17178d208db48daec088d80ba0e9b949355ed447
danyscor13-netizen <danyscor13@gmail.com>
2025-07-27 15:30:37 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, join_room, send
+
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+    
+@app.route('/chat', methods=['POST'])
+def chat():
+    username = request.form['username']
+    room = request.form['room']
+    return render_template('chat.html', username=username, room=room)
+    
+@socketio.on('join')
+def handle_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send({'msg': f"{username} è entrato nella chat!"}, room=room)
+    
+@socketio.on('message')
+def handle_messages(data):
+    username = data['username']
+    room = data['room']
+    msg = data['msg']
+    send({'username' : username, 'msg' : msg}, room=room)
+    
+if __name__ == "__main__":
+    socketio.run(app)
